cd64e31018dfded2aeaa1b116bc3fd2fe2a71a45
Maksim <143986439+AnkumaGithub@users.noreply.github.com>
2024-09-15 19:05:44 +0300

    Create 978a.cpp

---
+++ prj.codeforces/978a.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cmath>
+#include <algorithm>
+ 
+int main() {
+	int n;
+	std::cin >> n;
+	std::vector<int> A(n);
+	std::vector<int> data(1001);
+	std::vector<int> B;
+	for (int i = 0; i < n; i++) {
+		std::cin >> A[i];
+		data[A[i]] = 1;
+	}
+	for (int j = n - 1; j >= 0; j--) {
+		if (data[A[j]] == 1) {
+			B.push_back(A[j]);
+			data[A[j]] = 0;
+		}
+	}
+	std::cout << B.size() << std::endl;
+	for (int g = B.size() - 1; g >= 0; g--) {
+		std::cout << B[g] << " ";
+	}
+}
