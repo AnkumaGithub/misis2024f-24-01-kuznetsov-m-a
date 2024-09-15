c196c9d85fe22e211806e314e325cd1052d3c4a4
Maksim <143986439+AnkumaGithub@users.noreply.github.com>
2024-09-15 19:04:52 +0300

    Create 1872A.cpp

---
+++ prj.codeforces/1872A.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cmath>
+#include <algorithm>
+ 
+int main() {
+	int qua, a, b, c;
+	std::cin >> qua;
+	for (int i = 0; i < qua; i++) {
+		std::cin >> a >> b >> c;
+		int f = 0, k = 0;
+		while (f == 0 && a != b) {
+			if (abs(a - b) <= c * 2) {
+				k += 1;
+				f = 1;
+			}
+			else {
+				k += 1;
+				if (a >= b) {
+					a -= c;
+					b += c;
+				}
+				else {
+					b -= c;
+					a += c;
+				}
+			}
+		}
+		std::cout << k << std::endl;
+	}
+}
