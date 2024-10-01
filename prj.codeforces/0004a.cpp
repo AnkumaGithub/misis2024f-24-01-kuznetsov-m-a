969a4edda49386ac8b0b713ede3e3f0686d3c758
Maksim <143986439+AnkumaGithub@users.noreply.github.com>
2024-10-01 21:17:27 +0300

    Create 0004a.cpp

---
+++ prj.codeforces/0004a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+    int m = 0;
+    std::cin >> m;
+    if (0 == m % 2 && m != 2) {
+        std::cout << "YES";
+    } else {
+        std::cout << "NO";
+        
+    }
+}
